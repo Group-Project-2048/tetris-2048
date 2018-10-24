31d295911b1e8f375cafe148540a3f15d251ef07
Jose <joseangelortiz15@gmail.com>
2018-10-24 12:28:16 -0600

    show players

--- server/server.js
+++ server/server.js
@@ -2,8 +2,9 @@ require('dotenv').config()
 const session = require('express-session');
 const massive = require('massive');
 const bodyParser = require('body-parser');
+const controller = require('./controller');
 const express = require('express');
-const axios = require('axios');
+// const axios = require('axios');
 const app = express();
 
 
@@ -12,6 +13,8 @@ const {
     SERVER_PORT,
     CONNECTION_STRING,
 } = process.env;
+
+app.use(bodyParser.json())
 
 app.use(session({
     secret: SESSION_SECRET,
@@ -24,9 +27,14 @@ massive(CONNECTION_STRING).then(dbInstance=> {
     console.log('Database Connected')
 }).catch(err => console.log(err));
 
-app.use(bodyParser.json())
 
 
 //Endpoints
+
+// let c = controller;
+
+//Get all players into the players array in the leaderboard state
+
+app.get('/api/getPlayers', controller.getPlayers);
 
 app.listen(SERVER_PORT, () => {console.log(`Server ${SERVER_PORT} is running`); })
