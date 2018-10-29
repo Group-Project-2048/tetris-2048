fb9d518b8b2e7914d9c560efb2c5d05cd7e79aff
Digory Ellsworth <ellsworth.digory@gmail.com>
2018-10-29 08:54:12 -0600

    Final idea for pause menu

--- src/Components/Pause/Pause.js
+++ src/Components/Pause/Pause.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { withRouter, Link } from 'react-router-dom';
-import './Pause.scss';
+// import './Pause.scss';
+import './Pause.css';
 
 // card colors from the solitaire game
 // 2 = #C0B4A5 4 = #F2CB35 8 = #FF891F 16 = #FF0A15 32 = #7DCB3E 64 = #C47BFB 128 = #FF019D 256 = #12B559 512 = #8500F5 1024 = #018EE6 2048 = #000000
@@ -24,49 +25,21 @@ class Pause extends Component {
 
     render(){
         if(this.props.location.pathname === '/'){
-            if(this.state.pause){
                 return(
-                    <frosted-glass-container stretch='true'>
-                        <div className='glass-container'>
-                            <frosted-glass overlay-color="#ffffff52" blur-amount="4px" class='nav-container'>
-                                <nav className='menu'>
-                                    <input onClick={this.handlePause} className='menu-toggler' id='menu-toggler' type='checkbox'/>
-                                    <label htmlFor='menu-toggler'></label>
-                                        <ul>
-                                            <li className='menu-item'>
-                                                <button>R</button>
-                                            </li>
-                                            <li className='menu-item'>
-                                                <button><Link to='/leader'>LB</Link></button>
-                                            </li>
-                                            <li className='menu-item'>
-                                                <button>S</button>
-                                            </li>
-                                        </ul>
-                                </nav>
-                            </frosted-glass>
+                    <nav role="navigation">
+                        <div id="menuToggle">
+                            <input type="checkbox" />
+                                <span></span>
+                                <span></span>
+                                <span></span>
+                                <ul id="menu">
+                                    <li>Restart</li>
+                                    <Link to='/leader'><li>Leader Boards</li></Link>
+                                    <li>Sound: On / Off</li>
+                                </ul>
                         </div>
-                    </frosted-glass-container>
-                )
-            } else {
-                return(
-                    <nav className='menu'>
-                        <input onClick={this.handlePause} className='menu-toggler' id='menu-toggler' type='checkbox'/>
-                        <label htmlFor='menu-toggler'></label>
-                            <ul>
-                                <li className='menu-item'>
-                                    <button onClick={this.handleStart}>R</button>
-                                </li>
-                                <li className='menu-item'>
-                                    <button><Link to='/leader'>LB</Link></button>
-                                </li>
-                                <li className='menu-item'>
-                                    <button>S</button>
-                                </li>
-                            </ul>
                     </nav>
                 )
-            }
         } else {
             return null
         }
@@ -74,3 +47,22 @@ class Pause extends Component {
 }
 
 export default withRouter(Pause);
+
+
+/* <div className='glass-container'>
+<nav className='menu'>
+    <input onClick={this.handlePause} className='menu-toggler' id='menu-toggler' type='checkbox'/>
+    <label htmlFor='menu-toggler'></label>
+        <ul>
+            <li className='menu-item'>
+                <button>R</button>
+            </li>
+            <li className='menu-item'>
+                <button><Link to='/leader'>LB</Link></button>
+            </li>
+            <li className='menu-item'>
+                <button>S</button>
+            </li>
+        </ul>
+</nav>
+</div> */
