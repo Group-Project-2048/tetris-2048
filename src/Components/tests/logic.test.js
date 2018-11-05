6e9f15764527ec6a5b6e4b4cb9da70c36c4d2307
hanfamilym1 <michael@hanfamily.com>
2018-11-05 09:17:41 -0700

    Boiler Plate for Unit Testing

---
+++ src/Components/tests/logic.test.js
@@ -0,0 +1,13 @@
+const {increaseScore} = require('../Logic/logic')
+
+describe("user score is increased", ()=>{
+    test('return a number', ()=>{
+        expect(increaseScore()).toBe(5)
+    })
+   
+
+
+
+
+
+})
